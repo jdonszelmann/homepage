c4c08874449fbb6daf82d7aac5669cf5ddc0011a
Jana Dönszelmann <jana@donsz.nl>
2025-10-27 08:58:38 +0100

    keys

--- flake.nix
+++ flake.nix
@@ -7,10 +7,6 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        npmDepsHash = "sha256-xhw+CutTTQzQCci+TUWifez0JofIYU+izySnlPfVBJE=";
-
-        nativeBuildInputs = with pkgs; [ ];
-        buildInputs = with pkgs; [ nodejs ];
 
         dev = with pkgs;
           writeScriptBin "dev" ''
@@ -20,37 +16,15 @@
           writeScriptBin "prefetch" ''
             nix run nixpkgs#prefetch-npm-deps package-lock.json
           '';
-        nerdfonts =
-          pkgs.nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; };
-        build = vars: name: (pkgs.buildNpmPackage {
-                  version = "22-05-2024";
-                  src = ./.;
-                  inherit nativeBuildInputs buildInputs npmDepsHash name;
-                  configurePhase = ''
-                    mkdir -p ./public/fonts
-                    ln -sf ${nerdfonts}/share/fonts/truetype/NerdFonts/* ./public/fonts/
-                    ln -sf ${pkgs.fira}/share/fonts/opentype/* ./public/fonts/
-                    ln -sf ${pkgs.jetbrains-mono}/share/fonts/truetype/* ./public/fonts/
-                    ln -sf ${pkgs.fira-mono}/share/fonts/opentype/* ./public/fonts/
-                  '';
-                  buildPhase = ''
-                    ${vars}
-                    ${pkgs.nodejs}/bin/npm run build
-                  '';
-                  installPhase = ''
-                    cp -pr dist $out
-                  '';
-                });
       in rec {
         packages = rec {
-          website = build "" "homepage";
-          website-gay = build "export GAY=1" "homepage-gay";
+          website = pkgs.callPackage ./packages/homepage.nix {name = "homepage";};
+          website-gay = pkgs.callPackage ./packages/homepage.nix {vars="export GAY=1"; name="homepage-gay";};
           default = website;
         };
         devShells.default = with pkgs; mkShell {
-          buildInputs = buildInputs ++ [ dev prefetch yarn ];
-          inherit nativeBuildInputs;
-          packages = with pkgs; [ lychee (pkgs.writeShellScriptBin "watch" ''
+          buildInputs = [ nodejs dev prefetch yarn ];
+          packages = [ lychee (pkgs.writeShellScriptBin "watch" ''
             yarn run dev --host '0.0.0.0:8000'
           '') ];
           shellHook = packages.website.configurePhase;
---
+++ packages/homepage.nix
@@ -0,0 +1,51 @@
+{
+  nodejs,
+  buildNpmPackage,
+  nerdfonts,
+  fira,
+  jetbrains-mono,
+  fira-mono,
+  fetchurl,
+  name,
+  vars ? "",
+}:
+let
+  nerdfonts-symbols = nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; };
+  npmDepsHash = "sha256-xhw+CutTTQzQCci+TUWifez0JofIYU+izySnlPfVBJE=";
+  keys = fetchurl {
+    url = "https://github.com/jdonszelmann.keys";
+    sha256 = "sha256:1sla88pmh16jg9zx7kacca96599j38b2c340hlkdvzgjpclys28c";
+  };
+in
+(buildNpmPackage {
+  version = "22-05-2024";
+  src = ../.;
+  nativeBuildInputs = [ ];
+  buildInputs = [ nodejs ];
+  inherit npmDepsHash name;
+  configurePhase = ''
+    mkdir -p ./public/fonts
+    mkdir -p ./src/components
+
+    ln -sf ${nerdfonts-symbols}/share/fonts/truetype/NerdFonts/* ./public/fonts/
+    ln -sf ${fira}/share/fonts/opentype/* ./public/fonts/
+    ln -sf ${jetbrains-mono}/share/fonts/truetype/* ./public/fonts/
+    ln -sf ${fira-mono}/share/fonts/opentype/* ./public/fonts/
+
+    cat > Keys.astro <<EOF
+    ---
+    ---
+    <ul class="keys">
+    EOF
+    cat ${keys} | xargs -I{} echo "<li>{}</li>" >> Keys.astro
+    echo "</ul>" >> Keys.astro
+    mv Keys.astro ./src/components/Keys.astro
+  '';
+  buildPhase = ''
+    ${vars}
+    ${nodejs}/bin/npm run build
+  '';
+  installPhase = ''
+    cp -pr dist $out
+  '';
+})
