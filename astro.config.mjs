be269da98e8193684e7e4f192b8bfb0a768e8dc1
Jana Dönszelmann <jana@donsz.nl>
2025-11-24 18:13:50 +0100

    fix typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
-import { defineConfig } from 'astro/config';
-import expressiveCode from "astro-xpressive-code";
-import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
+import { defineConfig } from "astro/config";
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 import mdx from "@astrojs/mdx";
 
-
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    site: 'https://donsz.nl/',
-    integrations: [expressiveCode({
-        plugins: [pluginLineNumbers()],
-        themes: ['github-dark'],
-        styleOverrides: {
-            codeFontFamily: "JBM"
-        }
-    }), mdx()]
+  output: "static",
+  site: "https://donsz.nl/",
+  integrations: [
+    expressiveCode({
+      plugins: [pluginLineNumbers()],
+      themes: ["github-dark"],
+      styleOverrides: {
+        codeFontFamily: "JBM",
+      },
+    }),
+    mdx(),
+  ],
 });
