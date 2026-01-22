5a42f1b1770e7aa9cca393d1906eb9d09b4a4078
Jana Dönszelmann <jana@donsz.nl>
2026-01-22 21:52:28 +0100

    music blog posts (and a window to the past)

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,13 +5,14 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     tags: z.array(z.string()),
-    reviewers: z.array(z.string()),
+    reviewers: z.array(z.string()).optional(),
     authors: z.array(z.string()),
     description: z.string(),
     draft: z.boolean(),
     time: z.string().optional(),
     pubDate: z.date().optional(),
     ligatures: z.boolean().optional(),
+    variant: z.string().optional(),
   }),
 });
 
